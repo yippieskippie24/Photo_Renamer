90c697bdbb6800bdac9cea01a6041a191b2303d2
Tyler M Johnson <github@myinternet.tools>
2023-05-03 16:39:02 -0500

    Added Dependency checking and moved items
    Moved a number of variables that were in the rename for loop up into the image_files list loop.

--- Photo_import.py
+++ Photo_import.py
@@ -5,15 +5,17 @@ import subprocess
 import logging
 from datetime import datetime, date
 
-source_dir = "/Users/tmj/Pictures/Import" # Replace with the path to your folder
-destination_dir = "/Users/tmj/Pictures/Import/Organize_Photos"
+# Set the source and destination directories, as well as the desired filename prefix
+source_dir = "/Users/tmj/Downloads/import_test" # Replace with the path to your folder
+destination_dir = "/Users/tmj/Downloads/import_test/Organize_Photos"
 prefix = "TMJ" # Replace with your desired prefix
+# Set the starting sequence number and initialize variables for tracking previous capture time/date and sequence number
 sequence_number = 1 # Starting sequence number
 prev_capture_date = None # Initialize previous capture date
 prev_capture_time = None # Initialize previous capture time
 prev_sequence_number = None # Initialize previous sequence number
 
-# Camera ID map used to specificaly set the names I want in the files based on the camera make in the EXIF data
+# Map of camera brands to desired camera ID strings (used in filenames)
 camera_id_map = {
     "Canon": "CANON",
     "FUJIFILM": "FUJI",
@@ -26,7 +28,24 @@ log_file = f"{source_dir}/renaming.log"
 logging.basicConfig(filename=log_file, level=logging.INFO,
                     format="%(asctime)s - %(levelname)s - %(message)s")
 
-# Get list of image files and sort by capture time
+# Check for dependencies
+def check_dependencies():
+    dependencies = ['exiftool']
+    missing_dependencies = []
+    for dependency in dependencies:
+        try:
+            subprocess.check_output([dependency, '-ver'])
+        except subprocess.CalledProcessError:
+            missing_dependencies.append(dependency)
+    if missing_dependencies:
+        print(f"WARNING: The following dependencies are missing: {', '.join(missing_dependencies)}")
+        print("Please install the missing dependencies before running the script.")
+        sys.exit(1)
+        
+check_dependencies()
+
+
+# Get list of image files in the source directory, filter out non-image files
 image_files = []
 for filename in os.listdir(source_dir):
     if filename.lower().endswith((".jpg", ".jpeg", ".cr2", ".cr3", ".raf", ".arw")):
@@ -38,7 +57,18 @@ for filename in os.listdir(source_dir):
             capture_time_str = output.split(":", 1)[1].strip()
             capture_time = datetime.strptime(capture_time_str.split("-")[0].strip(), "%Y:%m:%d %H:%M:%S")
             capture_date = capture_time.date()
-            image_files.append((filename, img_path, capture_time, capture_date))
+            file_extension = os.path.splitext(filename)[1]
+            camera_brand = None
+            # Get camera brand from exif data using exiftool
+            cmd = ["exiftool", "-s", "-Make", img_path]
+            output = subprocess.check_output(cmd).decode().strip()
+            camera_make = output.split(":", 1)[1].strip()
+            logging.info(f"camera_make: {camera_make}")
+            
+            # Get the desired camera ID string for the current image based on its camera brand (using the camera_id_map)
+            camera_id = camera_id_map.get(camera_make, "Unknown")
+            
+            image_files.append((filename, img_path, capture_time, capture_date, file_extension, camera_id))
         except Exception as e:
             logging.error(f"Error processing {filename}: {e}")
 
@@ -46,18 +76,8 @@ for filename in os.listdir(source_dir):
 image_files.sort(key=lambda x: x[2])
 
 # Rename image files
-for filename, img_path, capture_time, capture_date in image_files:
+for filename, img_path, capture_time, capture_date, file_extension, camera_id in image_files:
     try:
-        file_extension = os.path.splitext(filename)[1]
-        camera_brand = None
-        # Get camera brand from exif data using exiftool
-        cmd = ["exiftool", "-s", "-Make", img_path]
-        output = subprocess.check_output(cmd).decode().strip()
-        camera_make = output.split(":", 1)[1].strip()
-        logging.info(f"camera_make: {camera_make}")
-        
-        camera_id = camera_id_map.get(camera_make, "Unknown")
-        
         # Reset sequence number if first image of a new day
         if prev_capture_date != capture_date:
             prev_capture_date = capture_date
