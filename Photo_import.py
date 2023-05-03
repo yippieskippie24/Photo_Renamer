3abdcdaa84ddf7369e2cfefbe50e8f9c8b127f65
Tyler M Johnson <github@myinternet.tools>
2023-05-03 17:11:16 -0500

    Rename changed to function
    I changed the rename section of the script to a function instead of general for loop.

--- Photo_import.py
+++ Photo_import.py
@@ -9,11 +9,6 @@ from datetime import datetime, date
 source_dir = "/Users/tmj/Downloads/import_test" # Replace with the path to your folder
 destination_dir = "/Users/tmj/Downloads/import_test/Organize_Photos"
 prefix = "TMJ" # Replace with your desired prefix
-# Set the starting sequence number and initialize variables for tracking previous capture time/date and sequence number
-sequence_number = 1 # Starting sequence number
-prev_capture_date = None # Initialize previous capture date
-prev_capture_time = None # Initialize previous capture time
-prev_sequence_number = None # Initialize previous sequence number
 
 # Map of camera brands to desired camera ID strings (used in filenames)
 camera_id_map = {
@@ -63,43 +58,62 @@ for filename in os.listdir(source_dir):
             cmd = ["exiftool", "-s", "-Make", img_path]
             output = subprocess.check_output(cmd).decode().strip()
             camera_make = output.split(":", 1)[1].strip()
-            logging.info(f"camera_make: {camera_make}")
+            logging.debug(f"camera_make: {camera_make}")
             
             # Get the desired camera ID string for the current image based on its camera brand (using the camera_id_map)
             camera_id = camera_id_map.get(camera_make, "Unknown")
-            
+            logging.debug(f"camera_id: {camera_id}")
             image_files.append((filename, img_path, capture_time, capture_date, file_extension, camera_id))
         except Exception as e:
             logging.error(f"Error processing {filename}: {e}")
 
+logging.info(f"image_files: {image_files}")
 # Sort image files by capture time
 image_files.sort(key=lambda x: x[2])
 
-# Rename image files
-for filename, img_path, capture_time, capture_date, file_extension, camera_id in image_files:
-    try:
-        # Reset sequence number if first image of a new day
-        if prev_capture_date != capture_date:
-            prev_capture_date = capture_date
-            sequence_number = 1
-        
-        logging.info(f"Filename {filename}")
-        logging.info(f"prev_capture_time: {prev_capture_time}")
-        logging.info(f"Sequence_number: {sequence_number}")
-        
-        # Assign sequence number to current file
-        if prev_capture_time is not None and prev_capture_time == capture_time:
-            sequence_number -= 1
-        prev_capture_time = capture_time
-        
-        
-        # Rename File useing the info gathered and move to the soure directory
-        new_filename = f"{prefix}_{capture_time.strftime('%y%m%d')}_{camera_id}_{str(sequence_number).zfill(4)}{file_extension}"
-        os.rename(img_path, os.path.join(destination_dir, new_filename))
-        logging.info(f"Renamed {filename} to {new_filename}")
-        sequence_number += 1
-    except Exception as e:
-        logging.error(f"Error renaming {filename}: {e}")
-
+# Rename image files function
+def rename_image_files(image_files, camera_id_string):
+    # Set the starting sequence number and initialize variables for tracking previous capture time/date and sequence number
+    sequence_number = 1 # Starting sequence number
+    prev_capture_date = None # Initialize previous capture date
+    prev_capture_time = None # Initialize previous capture time
+    prev_sequence_number = None # Initialize previous sequence number
     
+    for filename, img_path, capture_time, capture_date, file_extension, camera_id in image_files:
+        try:
+            # Check if the file's camera_id matches the provided value
+            if camera_id_string != camera_id:
+                continue
+            
+            # Reset sequence number if first image of a new day
+            if prev_capture_date != capture_date:
+                prev_capture_date = capture_date
+                sequence_number = 1
+            
+            logging.debug(f"Filename {filename}")
+            logging.debug(f"prev_capture_time: {prev_capture_time}")
+            logging.debug(f"Sequence_number: {sequence_number}")
+            
+            # Assign sequence number to current file
+            if prev_capture_time is not None and prev_capture_time == capture_time:
+                sequence_number -= 1
+            prev_capture_time = capture_time
+            
+            
+            # Rename File useing the info gathered and move to the soure directory
+            new_filename = f"{prefix}_{capture_time.strftime('%y%m%d')}_{camera_id}_{str(sequence_number).zfill(4)}{file_extension}"
+            os.rename(img_path, os.path.join(destination_dir, new_filename))
+            logging.info(f"Renamed {filename} to {new_filename}")
+            sequence_number += 1
+
+
+        except Exception as e:
+            logging.error(f"Error renaming {filename}: {e}")
+
+            
+# Run the rename image function for each of the camera_is listed in the camera_id_map
+for camera_id_string in camera_id_map.values():
+    rename_image_files(image_files, camera_id_string)
+
+
 logging.info("Renaming complete.")
